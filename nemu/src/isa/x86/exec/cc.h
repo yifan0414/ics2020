67119ed9757cdb8391356ef6362ff35dc5f860d9
suyifan <1679634762@qq.com>
2023-07-07 15:37:30 +0800

    PA2: Add 0x0f set instruction

--- nemu/src/isa/x86/exec/cc.h
+++ nemu/src/isa/x86/exec/cc.h
@@ -25,18 +25,19 @@ static inline const char* get_cc_name(int subcode) {
 
 static inline void rtl_setcc(DecodeExecState *s, rtlreg_t* dest, uint32_t subcode) {
   uint32_t invert = subcode & 0x1;
+  // 这里是因为 指令的分布是 SETO SETNO SETB SETNB, 因此我们只需要判断最后以为是否为1即可来区分是否需要取反
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
+  // 1110
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-       TODO();
+    case CC_O: if (cpu.eflags.OF == 1) *dest = 1; else *dest = 0; break;
+    case CC_B: if (cpu.eflags.CF == 1 && cpu.eflags.ZF == 0) *dest = 1; else *dest = 0; break;
+    case CC_E: if (cpu.eflags.ZF == 1) *dest = 1; else *dest = 0; break;
+    case CC_BE:if (cpu.eflags.CF == 1 || cpu.eflags.ZF == 1) *dest = 1; else *dest = 0; break;
+    case CC_S: if (cpu.eflags.SF == 1) *dest = 1; else *dest = 0; break;
+    case CC_L: if (cpu.eflags.SF != cpu.eflags.OF && cpu.eflags.ZF == 0) *dest = 1; else *dest = 0; break;
+    case CC_LE:if (cpu.eflags.SF != cpu.eflags.OF || cpu.eflags.ZF == 1) *dest = 1; else *dest = 0; break;
     case CC_P: panic("PF is not supported");
     default: panic("should not reach here");
   }
